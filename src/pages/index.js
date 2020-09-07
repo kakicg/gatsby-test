e2e7e6207f4d566cab8ea8fe6a51e699cac29913
柿山幹雄 <kakicg@cgkojo.com>
2020-09-07 23:34:34 +0900

    Blog List

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 import Img from "gatsby-image"
 
 import Layout from "../components/layout"
@@ -79,6 +79,29 @@ export default ({ data }) => (
         />
       </figure>
     </section>
+
+    <section>
+        <div className="container">
+          <h2 className="bar">RECENT POSTS</h2>
+          <div className="posts">
+              {data.allContentfulBlogPost.edges.map(({ node }) => (
+                <article className="post" key={node.id}>
+                <Link to={`/blog/post/${node.slug}/`}>
+                    <figure>
+                    <Img
+                        fluid={node.eyecatch.fluid} 
+                        alt={node.eyecatch.description}
+                        style={{ height: "100%" }}
+                    />
+                    </figure>
+                    <h3>{node.title}</h3>
+                </Link>
+                </article>
+              ))}
+          </div>
+        </div>
+    </section>
+
   </Layout>
 )
 
@@ -118,6 +141,25 @@ export const query = graphql`
           ...GatsbyImageSharpFluid_withWebp
         }
       }   
-    }   
+    }
+    allContentfulBlogPost(
+      sort: { order: DESC, fields: publishDate }
+      skip: 0
+      limit: 4
+    ) {
+        edges {
+            node {
+                title
+                id
+                slug
+                eyecatch {
+                    fluid(maxWidth: 573) {
+                        ...GatsbyContentfulFluid_withWebp
+                    }
+                    description
+                }
+            }
+        }
+    } 
   }
 `
